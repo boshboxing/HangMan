77b5dcf5047c3b0b5d07785116603a15a0dee8d7
Bashar Soufan <bsoufan5@gmail.com>
2024-07-10 11:55:24 +0300

    test

--- hangman.py
+++ hangman.py
@@ -135,7 +135,7 @@ def display_hangman(tries):
 def main():
     word = get_word()
     play(word)
-    while input("Play Again? (Y/N) ").upper() == "Y":
+    while input("PlayAgain? (Y/N) ").upper() == "Y":
         word = get_word()
         play(word)
 
